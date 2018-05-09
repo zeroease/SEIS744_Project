96ccd506ee6f8073ba56a5fe31effe06266ce3d9
Antonio DiTommaso <zeroease@yahoo.com>
2018-05-09 08:28:04 -0500

    Update project.ino

--- Project/project.ino
+++ Project/project.ino
@@ -1,20 +1,24 @@
 //ANTONIO DITOMMASO PROJECT FOR SEIS 744-01
 //CODE PULLED FROM https://particle.hackster.io/Richa1/kid-tracker-1b58c9
 // This #include statement was automatically added by the Particle IDE.
+// add these libraries to you code to allow this functions to automatically be added by the Particle IDE.
 #include "application.h"
 #include "neopixel/neopixel.h"
 #include "TinyGPS/TinyGPS.h"
 
+
 SYSTEM_MODE(AUTOMATIC);
 
-// IMPORTANT: Set pixel COUNT, PIN and TYPE
-#define PIXEL_PIN D7
-#define PIXEL_COUNT 20
-#define PIXEL_TYPE WS2812B
-#define HELP_BUTTON D5
+
+// IMPORTANT Set values
+#define PIXEL_PIN D7 //Set Pixel to Pin D7 on particle
+#define PIXEL_COUNT 20 //Set Pixel Count to 20
+#define PIXEL_TYPE WS2812B //Set the Pixel Type to WS2812B
+#define HELP_BUTTON D5 //Set the active button to D5
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
 
+//Varibles
 TinyGPS gps;
 char szInfo[64];
 int show = 1;
@@ -26,7 +30,7 @@ int FadeG;
 int FadeB;
 int wait = 14000;
 
-
+//Particle setup, initialize certain functions on Particle core
 void setup()
 {
     strip.begin();
@@ -38,25 +42,28 @@ void setup()
     
 }
 
-void loop(){
 
+//Particle Loop, inside is heart of code
+void loop(){
+//Create Cases that allow your code to work based on certain conditions
       switch (show) {
-    case 1:    // your hand is nowhere near the sensor
+    case 1:    
       Sparkle(50);
       break;
-    case 2:    // your hand is close to the sensor
+    case 2: //Case 2 sets color of that partical case
       colorWipe(strip.Color(R, G, B), wait);
       colorWipe(strip.Color(0, 0, 0), wait);
       break;
-    case 3:    // your hand is close to the sensor
+    case 3:  //Case 3 sets color of that partical case
       colorWipe(strip.Color(R, G, B), wait);
       colorWipe(strip.Color(0, 0, 0), wait);
       break;
-    case 4:    // your hand is close to the sensor
+    case 4:  //Case 4 sets color of that partical case, also, set a valid GPS indicator
       colorWipe(strip.Color(R, G, B), wait);
       colorWipe(strip.Color(0, 0, 0), wait);
       bool isValidGPS = false;
     
+    //See if GPS is working
     for (unsigned long start = millis(); millis() - start < 1000;){
         // Check GPS data is available
         while (Serial1.available()){
@@ -66,25 +73,29 @@ void loop(){
             if (gps.encode(c))
                 isValidGPS = true;
         }
+        
+        
     }
 
-    // If we have a valid GPS location then publish it
+    // If statement that sees if we have a valid GPS location then publish it
     if (isValidGPS){
         float lat, lon;
         unsigned long age;
     
         gps.f_get_position(&lat, &lon, &age);
-
+        //GPS format, allows the gps signal to be set Latitude, Longitude
         sprintf(szInfo, "%.6f,%.6f", (lat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : lat), (lon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : lon));
 
     }
-    
+    //Publish the data, so other patforms can use
     Particle.publish("gpsloc", szInfo);
       break;
   }
   delay(1);        // delay in between reads for stability
 
 }
+
+//If statements to get commands to change LED colors
 int ledToggle(String command) {
     if (command=="Play") {
         colorAll(strip.Color(0, 0, 0), 0);
@@ -153,7 +164,7 @@ void colorWipe(uint32_t c, uint8_t wait) {
     delay(wait);
   }
 }
-
+//if command to allow the LED to pulse light differently
 void Sparkle(int t){
     for(uint16_t i=0; i<strip.numPixels(); i++) {
         if (random(3000) < t) {
@@ -183,3 +194,4 @@ void Sparkle(int t){
       }
     strip.show();
 }
+
